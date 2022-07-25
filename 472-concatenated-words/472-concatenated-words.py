022e9de8bb6962a2fafec966f4adf30e3b31aa3d
Nishant Sulay <nishantsulay@hotmail.com>
2022-07-25 14:55:10 -0400

    Time: 724 ms (47.16%), Space: 27.9 MB (40.79%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.py
+++ 472-concatenated-words/472-concatenated-words.py
@@ -1,6 +1,7 @@
 class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
         
+        mem = {}
         hashset = set()
         
         for word in words:
@@ -15,12 +16,16 @@ class Solution:
             if word in hashset:
                 return true
             
+            if word in mem:
+                return True
+            
             for i in range(1,len(word)):
                                 
                 prefix = word[:i]
                 suffix = word[i:]
                 
                 if prefix in hashset and (suffix in hashset or isConcat(suffix)):
+                    mem[word] = True
                     return True
                 
         
@@ -35,3 +40,19 @@ class Solution:
        
         return output
     
+    
+    '''
+    catdog
+    
+    c in set and dfs(atdog)
+    ca in set and dfs(tdog)
+    cat in set and dfs(dog)
+    catd in set and dfs(og)
+    catdo in set and dfs(g)
+    
+    a in set and dfs(tdog)
+    at in set and dfs(dog)
+    atd in set and dfs(og)
+    atdo in set and dfs(g)
+    
+    '''
