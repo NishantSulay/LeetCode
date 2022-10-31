e98e4075291786083e4e0ba7d1306cc280f6f80b
Nishant Sulay <nishantsulay@hotmail.com>
2022-10-31 16:48:19 -0400

    Time: 114 ms, Memory: 14.5 MB - LeetHub

---
+++ valid-anagram/valid-anagram.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        '''
+        {a:0, n:0, g:0, r:0, m:0}
+        
+        O(1) - Memory
+        O(3N)- Time
+        '''
+        if len(s) != len(t):
+            return False
+        
+        hashMap = {}
+
+        for i in range(len(s)):
+            if s[i] in hashMap:
+                hashMap[s[i]] += 1
+            else:
+                hashMap[s[i]] = 1
+                
+        
+        for i in range(len(t)):
+            
+            if t[i] in hashMap:
+                hashMap[t[i]] -= 1
+            else:
+                hashMap[t[i]] = 1
+                
+        for index,key in enumerate(hashMap):
+            
+            if hashMap[key] != 0:
+                return False
+            
+        return True
+            
